0761e5b5566e367a8f52283ef7e921c6cf76617d
msssvitlana <missis.chuvaeva@ukr.net>
2025-04-02 08:28:19 +0000

    Deploying to gh-pages from @ msssvitlana/goit-js-hw-10@6c79477e802a71ae9ff698534830185559ec0fba 🚀

---
+++ 1-timer.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{f as s}from"./assets/vendor-CZ4EWmTT.js";s("#datetime-picker",{enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1});function t(o){const e=Math.floor(o/864e5),n=Math.floor(o%864e5/36e5),r=Math.floor(o%864e5%36e5/6e4),c=Math.floor(o%864e5%36e5%6e4/1e3);return{days:e,hours:n,minutes:r,seconds:c}}console.log(t(2e3));console.log(t(14e4));console.log(t(2414e4));
+//# sourceMappingURL=1-timer.js.map
