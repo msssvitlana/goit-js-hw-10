59de65d16ed74c01bbea0889a7a292cc6554ced9
Svitlana <missis.chuvaeva@ukr.net>
2025-04-03 23:55:47 +0300

    timer changes

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -3,14 +3,11 @@ import 'flatpickr/dist/flatpickr.min.css';
 
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-import injectHTML from 'vite-plugin-html-inject';
 
 const startButton = document.querySelector('.btn-start');
 const timerFace = document.querySelector('.timer');
 const datePicker = document.querySelector('#datetime-picker');
 let userSelectedDate;
-// const timerValue = document.querySelector('[seconds]');
-// const timerLabel = document.querySelectorAll('.label');
 
 flatpickr(datePicker, {
   enableTime: true,
@@ -28,6 +25,7 @@ function validateDate(userSelectedDate) {
     iziToast.error({
       title: 'Error',
       message: 'Please choose a date in the future',
+      position: 'center',
     });
     startButton.disabled = true;
   } else {
@@ -39,19 +37,19 @@ class Timer {
   constructor({ onTick }) {
     this.isActive = false;
     this.onTick = onTick;
-    this.init;
-    // console.log(this.init);
     this.intervalId = null;
-  }
-
-  init() {
-    const time = this.convertMs(0);
-    this.onTick(time);
   }
 
   start() {
     if (this.isActive || !userSelectedDate) {
-      datePicker.disabled = true;
+      // Якщо дата не вибрана або таймер вже активний, не запускати
+
+      startButton.disabled = true;
+      iziToast.warning({
+        title: 'No date selected',
+        message: 'Please select a valid future date before starting the timer.',
+        position: 'center',
+      });
       return;
     }
 
@@ -61,7 +59,7 @@ class Timer {
       const currentTime = Date.now();
       const deltaTime = userSelectedDate - currentTime;
       if (deltaTime <= 0) {
-        clearInterval(this.intervalId); // зупинка таймера, коли час вичерпано
+        clearInterval(this.intervalId); // Зупинка таймера, коли час вичерпано
         this.onTick({ days: '00', hours: '00', minutes: '00', seconds: '00' });
         this.isActive = false;
         datePicker.disabled = false;
@@ -70,7 +68,6 @@ class Timer {
           title: 'Time is up!',
           message: 'The countdown has finished.',
         });
-
         return;
       }
 
@@ -80,21 +77,16 @@ class Timer {
   }
 
   convertMs(ms) {
-    // Number of milliseconds per unit of time
     const second = 1000;
     const minute = second * 60;
     const hour = minute * 60;
     const day = hour * 24;
 
-    // Remaining days
     const days = this.addLeadingZero(Math.floor(ms / day));
-    // Remaining hours
     const hours = this.addLeadingZero(Math.floor((ms % day) / hour));
-    // Remaining minutes
     const minutes = this.addLeadingZero(
       Math.floor(((ms % day) % hour) / minute)
     );
-    // Remaining seconds
     const seconds = this.addLeadingZero(
       Math.floor((((ms % day) % hour) % minute) / second)
     );
@@ -112,7 +104,7 @@ const time = new Timer({
 });
 
 startButton.addEventListener('click', time.start.bind(time));
+
 function updateTimer({ days, hours, minutes, seconds }) {
   timerFace.textContent = `${days}:${hours}:${minutes}:${seconds}`;
 }
-console.log(time);
